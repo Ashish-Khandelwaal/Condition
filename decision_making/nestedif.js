658a74aa4ba2e010ea79d61066de0866c80b18ab
Ashish Khandelwal <khandelwalashish009@gmail.com>
2025-01-20 19:11:23 +0530

    add file nestedif

---
+++ decision_making/nestedif.js
@@ -0,0 +1,57 @@
+let subscriptionStatus = "Subscribed";
+let lionsgateplaySubscription = "No";
+let sonyPicturesSubscription = "No";
+
+// if (subscriptionStatus == "Subscribed") {
+//   if (lionsgateplaySubscription == "Yes") {
+//     console.log("User has Lionsgate Play Subscription.");
+//   } else if ((sonyPicturesSubscription = "Yes")) {
+//     console.log("User has Sony Pictures Subscription.");
+//   } else {
+//     console.log("User does not have Lionsgate play Subscription.");
+//   }
+// } else {
+//   console.log("User does not have subscription of our Services.");
+// }
+
+// if (subscriptionStatus == "Subscribed") {
+//     if ( lionsgateplaySubscription = "yes")
+//         console.log("User is subscribed to Lions gate play services.");
+// } else {
+//     console.log("User is not subscribed to Lions gate play services.");
+
+// } {
+//     if ( sonyPicturesSubscription = "yes") {
+//         console.log("User is subscribed to Sony Pictures services." );
+//     } else {
+//         console.log("User is not subscribed to Sony Pictures services.");
+
+//     }
+// } else {
+//     console.log(""User is not subscribed to our services."");
+
+// }
+
+if (subscriptionStatus == "Subscribed") {
+  console.log("User is Subscribed to Prime Services.");
+} else {
+  console.log("User is not Subscribed to Prime Services.");
+}
+
+if (subscriptionStatus == "Subscribed") {
+  if (lionsgateplaySubscription == "Yes") {
+    console.log("User is subscribed to Lions gate play services.");
+  }
+}
+
+if (subscriptionStatus == "Subscribed") {
+  if (sonyPicturesSubscription == "Yes") {
+    console.log("User is subscribed to Sony Pictures.");
+  }
+}
+
+// if (subscriptionStatus == "Subscribed") {
+//   if (sonyPicturesSubscription == "Yes") {
+//     console.log("User is subscribed to Sony gate play services.");
+//   }
+// }
