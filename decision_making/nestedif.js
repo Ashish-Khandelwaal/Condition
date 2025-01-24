2f0451a4d942286875d925c205377fa622b18fe3
Ashish Khandelwal <khandelwalashish009@gmail.com>
2025-01-24 22:12:28 +0530

    Update nestedif.js

--- decision_making/nestedif.js
+++ decision_making/nestedif.js
@@ -1,11 +1,11 @@
-let subscriptionStatus = "Subscribed";
-let lionsgateplaySubscription = "No";
-let sonyPicturesSubscription = "No";
+let subscriptionStatus = "Unsubscribed";
+let lionsgateplaySubscription = "Yes";
+let sonyPicturesSubscription = "Yes";
 
 if (subscriptionStatus == "Subscribed") {
   console.log("User is Subscribed to Prime Services.");
 } else {
-  console.log("User is not Subscribed to Prime Services.");
+  console.log("User is not Subscribed to Our Services.");
 }
 
 if (subscriptionStatus == "Subscribed") {
@@ -19,41 +19,3 @@ if (subscriptionStatus == "Subscribed") {
     console.log("User is subscribed to Sony Pictures.");
   }
 }
-
-// if (subscriptionStatus == "Subscribed") {
-//   if (lionsgateplaySubscription == "Yes") {
-//     console.log("User has Lionsgate Play Subscription.");
-//   } else if ((sonyPicturesSubscription = "Yes")) {
-//     console.log("User has Sony Pictures Subscription.");
-//   } else {
-//     console.log("User does not have Lionsgate play Subscription.");
-//   }
-// } else {
-//   console.log("User does not have subscription of our Services.");
-// }
-
-// if (subscriptionStatus == "Subscribed") {
-//     if ( lionsgateplaySubscription = "yes")
-//         console.log("User is subscribed to Lions gate play services.");
-// } else {
-//     console.log("User is not subscribed to Lions gate play services.");
-
-// } {
-//     if ( sonyPicturesSubscription = "yes") {
-//         console.log("User is subscribed to Sony Pictures services." );
-//     } else {
-//         console.log("User is not subscribed to Sony Pictures services.");
-
-//     }
-// } else {
-//     console.log(""User is not subscribed to our services."");
-
-// }
-
-
-
-// if (subscriptionStatus == "Subscribed") {
-//   if (sonyPicturesSubscription == "Yes") {
-//     console.log("User is subscribed to Sony gate play services.");
-//   }
-// }
